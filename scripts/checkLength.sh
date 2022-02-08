abf2a00cb19c2a72ca52aa650ce007690abb28f5
Oleksii Yesin <Oleksii_Yesin@epam.com>
2022-02-08 02:16:53 +0200

    Updated files

--- scripts/checkLength.sh
+++ scripts/checkLength.sh
@@ -4,8 +4,8 @@ message=`git log --format=%s%b -n 1`
 
 if [ ${#message} -gt 70 ]; 
   then 
-    echo "This message doesn't match the length"
+    echo "This message of latest commit doesn't match the length"
   else 
-    echo "This message does match the length"
+    echo "This message of latest commit does match the length"
 fi
 
